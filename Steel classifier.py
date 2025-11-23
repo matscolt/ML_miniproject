aeceae725ed43588d0e7fb98e1ad08f63b46ded8
Matscolt <mathias.terndrup@live.dk>
2025-11-23 13:49:08 +0100

    added the base file

---
+++ Steel classifier.py
@@ -0,0 +1,34 @@
+#Packages
+import pandas as pd
+
+
+#reading the data
+
+
+
+
+
+#data preprocessing
+
+
+
+
+#model building
+
+
+
+
+
+#model training 
+
+
+
+
+
+#Sorting and evaluating the results
+
+
+
+
+
+
